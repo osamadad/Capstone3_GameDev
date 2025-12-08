9fffdabf100e1d9788278e7876bd63c1b1404e3d
osama dad <osamadad768@gmail.com>
2025-12-08 17:50:41 +0300

    feat: api response/exception and controller advice

---
+++ src/main/java/com/tuwaiq/capstone3_gamedev/Advice/ControllerAdvice.java
@@ -0,0 +1,64 @@
+package com.tuwaiq.capstone3_gamedev.Advice;
+import com.tuwaiq.capstone3_gamedev.Api.ApiException;
+import com.tuwaiq.capstone3_gamedev.Api.ApiResponse;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@org.springframework.web.bind.annotation.ControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<?> ApiException(ApiException apiException){
+        return ResponseEntity.status(400).body(new ApiResponse(apiException.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> MethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
+        return ResponseEntity.status(400).body(new ApiResponse(methodArgumentNotValidException.getFieldError().getDefaultMessage()));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> ConstraintViolationException(ConstraintViolationException constraintViolationException) {
+        return ResponseEntity.status(400).body(new ApiResponse(constraintViolationException.getMessage()));
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<?> SQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException sqlIntegrityConstraintViolationException){
+        return ResponseEntity.status(400).body(new ApiResponse(sqlIntegrityConstraintViolationException.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidDataAccessResourceUsageException.class )
+    public ResponseEntity<?> InvalidDataAccessResourceUsageException(InvalidDataAccessResourceUsageException invalidDataAccessResourceUsageException){
+        return ResponseEntity.status(400).body(new ApiResponse(invalidDataAccessResourceUsageException.getMessage()));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> DataIntegrityViolationException(DataIntegrityViolationException dataIntegrityViolationException){
+        return ResponseEntity.status(400).body(new ApiResponse(dataIntegrityViolationException.getMessage()));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<?> HttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException httpRequestMethodNotSupportedException) {
+        return ResponseEntity.status(400).body(new ApiResponse(httpRequestMethodNotSupportedException.getMessage()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> HttpMessageNotReadableException(HttpMessageNotReadableException httpMessageNotReadableException) {
+        return ResponseEntity.status(400).body(new ApiResponse(httpMessageNotReadableException.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> MethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException methodArgumentTypeMismatchException) {
+        return ResponseEntity.status(400).body(new ApiResponse(methodArgumentTypeMismatchException.getMessage()));
+    }
+
+}
