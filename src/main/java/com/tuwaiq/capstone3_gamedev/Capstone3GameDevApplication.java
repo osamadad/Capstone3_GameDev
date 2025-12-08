41c9f18f055eb233eb0879feb3cd9f35fa04f58e
osama dad <osamadad768@gmail.com>
2025-12-08 17:45:39 +0300

    initial commit

---
+++ src/main/java/com/tuwaiq/capstone3_gamedev/Capstone3GameDevApplication.java
@@ -0,0 +1,13 @@
+package com.tuwaiq.capstone3_gamedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capstone3GameDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Capstone3GameDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuwaiq/capstone3_gamedev/Capstone3GameDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuwaiq.capstone3_gamedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capstone3GameDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
