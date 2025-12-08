faf18066636c3db8d748f36e3e25d3714cd6f7fe
leenref <leenref4@gmail.com>
2025-12-09 00:32:43 +0300

    investor, investingRequest, userRequest

--- src/main/java/com/tuwaiq/capstone3_gamedev/Model/InvestingRequest.java
+++ src/main/java/com/tuwaiq/capstone3_gamedev/Model/InvestingRequest.java
@@ -1,6 +1,45 @@
 package com.tuwaiq.capstone3_gamedev.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class InvestingRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotEmpty(message = "Offer is required")
+    private String offer;
+
+    @NotEmpty
+    @Pattern(
+            regexp = "(?i)^(Pending|Accepted|Rejected)$",
+            message = "Status must be Pending, Accepted, or Rejected"
+    )
+    private String status;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private Project project;
+
+    @ManyToOne
+    @JsonIgnore
+    private Investor investor;
 
 }
---
+++ src/main/java/com/tuwaiq/capstone3_gamedev/Service/InvestorService.java
@@ -0,0 +1,46 @@
+package com.tuwaiq.capstone3_gamedev.Service;
+
+import com.tuwaiq.capstone3_gamedev.Api.ApiException;
+import com.tuwaiq.capstone3_gamedev.Model.Investor;
+import com.tuwaiq.capstone3_gamedev.Repository.InvestorRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class InvestorService {
+    private final InvestorRepository investorRepository;
+
+    public List<Investor> getAll() {
+        return investorRepository.findAll();
+    }
+
+    public void add(Investor investor) {
+        investor.setCreatedAt(LocalDateTime.now());
+        investorRepository.save(investor);
+    }
+
+    public void update(Integer id, Investor investor) {
+        Investor old = investorRepository.findInvestorById(id);
+
+        if(old==null){
+            throw new ApiException("Investor not found");
+        }
+
+        old.setFullName(investor.getFullName());
+        old.setEmail(investor.getEmail());
+        old.setPassword(investor.getPassword());
+        old.setAvailableBudget(investor.getAvailableBudget());
+        old.setFundedProjects(investor.getFundedProjects());
+
+        investorRepository.save(old);
+    }
+
+    public void delete(Integer id) {
+        investorRepository.deleteById(id);
+    }
+
+}
